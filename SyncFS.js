750be41396e919a37dded02789a5b08086c54edf
talbotwhite <twhite@roblox.com>
2017-06-30 10:10:58 -0700

    Style changes

--- SyncFS.js
+++ SyncFS.js
@@ -11,30 +11,28 @@ function path_get_stat(path) {
 }
 
 function mkdir(path) {
-	console.log("Create[DIR ]:",path);
-
+	console.log("Create[DIR ]:", path);
 	if (path_get_stat(path) == null) {
 		fs.mkdirSync(path);
 	}
 }
 
-function mkscript(path,type,contents) {
+function mkscript(path, type, contents) {
 	path = path + "." + type + Util.FSEXT_LUA;
-	console.log("Create[FILE]:",path);
+	console.log("Create[FILE]:", path);
 
 	if (path_get_stat() != null) {
 		fs.unlinkSync(path);
 	}
-	fs.writeFileSync(path,contents);
+	fs.writeFileSync(path, contents);
 }
 
-function obj_rTraversal(obj_path,obj) {
+function obj_rTraversal(obj_path, obj) {
 	for (var i = 0; i < obj.Children.length; i++) {
 		var itr_child = obj.Children[i];
-		var itr_child_path = path.resolve(obj_path,itr_child.Name);
+		var itr_child_path = path.resolve(obj_path, itr_child.Name);
 		if (Util.IsScript(itr_child.Type)) {
-			mkscript(itr_child_path,itr_child.Type,itr_child.Source);
-
+			mkscript(itr_child_path, itr_child.Type, itr_child.Source);
 			itr_child_path = itr_child_path + "." + itr_child.Type;
 			if (itr_child.Children.length > 0) {
 				mkdir(itr_child_path);
