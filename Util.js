577e45d0d569afd0603c88819a84a02aadbf3e38
talbotwhite <twhite@roblox.com>
2017-06-29 17:52:59 -0700

    Sync fixes

--- Util.js
+++ Util.js
@@ -1,13 +1,13 @@
 
 module.exports = {
-  RBXTYPE_MODULESCRIPT: "ModuleScript",
-  RBXTYPE_LOCALSCRIPT: "LocalScript",
-  RBXTYPE_SCRIPT: "Script",
-  FSEXT_LUA: ".lua",
+	RBXTYPE_MODULESCRIPT: "ModuleScript",
+	RBXTYPE_LOCALSCRIPT: "LocalScript",
+	RBXTYPE_SCRIPT: "Script",
+	FSEXT_LUA: ".lua",
 
-  IsScript: function(type) {
-    return type == this.RBXTYPE_MODULESCRIPT ||
-      type == this.RBXTYPE_LOCALSCRIPT ||
-      type == this.RBXTYPE_SCRIPT;
-  }
+	IsScript: function(type) {
+		return type == this.RBXTYPE_MODULESCRIPT ||
+			type == this.RBXTYPE_LOCALSCRIPT ||
+			type == this.RBXTYPE_SCRIPT;
+	}
 };
