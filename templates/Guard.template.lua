ffffb5aedd527ef88cb3a4adc606c7771be516c3
talbotwhite <twhite@roblox.com>
2017-08-02 11:03:31 -0700

    Fixed Guard

--- templates/Guard.template.lua
+++ templates/Guard.template.lua
@@ -1,1 +1,11 @@
-if game.PlaceId ~= %d then return end
+local validPlaceIds = %s
+local isValidPlaceId = false
+for _, placeId in pairs(validPlaceIds) do
+    if game.PlaceId == placeId then
+        isValidPlaceId = true
+    end
+end
+if not isValidPlaceId then
+    warn("[RbxRefresh] Bad game.PlaceId!")
+    return
+end
